d67c1295789ff15edbf9b1cb417aed3c793fc991
Preethithiyagarajansai <36123863+Preethithiyagarajansai@users.noreply.github.com>
2018-03-02 17:23:38 +0530

    Create moreonstrings.py

---
+++ Beginner/moreonstrings.py
@@ -0,0 +1,11 @@
+stringVar="programmer"
+loc=stringVar.count('r')
+print loc
+
+stringVar="programmer"
+loc=stringVar.find('g')
+print loc
+
+stringVar="programmer"
+loc=stringVar.replace('m','y')
+print loc
